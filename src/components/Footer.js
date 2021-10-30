e5e448fc87343f113c14f5c5bb4c059052e8a1d8
Lauri Järvinen <laurijarvinen@hotmail.com>
2021-10-30 17:07:33 +0300

    file setup

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="main-footer text-center text-lg-start bg-dark text-muted">
+            <section className="footer-main">
+                <div className="container text-center text-md-start">
+
+                </div>
+            </section>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="header-wrapper">
+            <h1>YEAA</h1>
+        </div>
+    )
+}
+
+export default Home;
